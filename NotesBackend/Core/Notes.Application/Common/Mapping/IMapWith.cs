43610ffaa93701e150e6cf4a0792fad0478ea900
kovalev70 <kovalev85291@gmail.com>
2024-01-22 23:59:33 +0300

    features(Notes.Application): mapping has been added.

---
+++ NotesBackend/Core/Notes.Application/Common/Mapping/IMapWith.cs
@@ -0,0 +1,27 @@
+﻿namespace Notes.Application.Common.Mapping
+{
+    using AutoMapper;
+    using System.Reflection;
+
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+
+        private void ApplyMappingFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                    .Any(i =>  i.IsGenericType &&
+                    i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+
+            foreach (var type in types) 
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] {this});
+            }
+        }
+    }
+}
