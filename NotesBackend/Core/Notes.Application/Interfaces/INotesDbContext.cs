29a9a654d7277cd0fe7c1a8866ffdfb0bcd6fc87
kovalev70 <kovalev85291@gmail.com>
2024-01-22 20:31:45 +0300

    feature(Core, Infrastructure): adding a database.

---
+++ NotesBackend/Core/Notes.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Notes.Application.Interfaces
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using Notes.Domain;
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ NotesBackend/Infrastructure/Notes.Persistence/NotesDbContext.cs
@@ -0,0 +1,21 @@
+﻿namespace Notes.Persistence
+{
+    using Microsoft.EntityFrameworkCore;
+    using Notes.Application.Interfaces;
+    using Notes.Domain;
+    using Notes.Persistence.EntityTypeConfigurations;
+
+    public class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        public NotesDbContext(DbContextOptions<NotesDbContext> options) 
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
