9ad3874a0f90a33b342df290fe394a802447a840
kovalev70 <kovalev85291@gmail.com>
2024-01-23 16:35:50 +0300

    features(Notes.Application, Notes.Persisternce): dependency Injection.

---
+++ NotesBackend/Infrastructure/Notes.Persistence/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿namespace Notes.Persistence
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Notes.Application.Interfaces;
+
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection 
+            services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<NotesDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<INotesDbContext>(provider =>
+                provider.GetService<NotesDbContext>());
+
+            return services;
+        }
+    }
+}
